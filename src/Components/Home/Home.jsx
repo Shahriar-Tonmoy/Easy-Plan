d2004eb844641394339c9e87d1cff16e850dcc1e
Tonmoy <shahariartonmoy17@gmail.com>
2023-12-22 05:07:15 +0600

    nav bar and banner has been added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -13,38 +13,39 @@ const Home = () => {
     AOS.init();
   }, [])
   return (
-    <div>
+    <div className="min-h-screen">
       <div
         className="hero h-[400px] md:h-[600px]"
         style={{
-          backgroundImage: "url('https://i.ibb.co/1bvQ0GF/11125.jpg')",
+          backgroundImage: "url('https://i.ibb.co/gJrd9zv/home.jpg')",
         }}
       >
         <div className="hero-overlay bg-black bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="">
             <h1 className="mb-5 text-5xl font-bold text-white ">
-            Discover Your Next Ride with <span className="text-[#00ADB5]">Next Wheels</span>
+            <span className="text-[#00ADB5]">EasyPlan</span>, Streamlining your workflow with seamless efficiency 
+            {/* <span className="text-[#00ADB5]">Next Wheels</span> */}
             </h1>
-            <p className="hidden md:block">Welcome to Next Wheels, your premier destination for automotive excellence! Whether you're a car enthusiast, a first-time buyer, or looking to upgrade your current vehicle, we've got you covered. With a wide range of top-notch automotive brands and models to choose from, we make finding your dream car a breeze. Our commitment to quality, transparency, and customer satisfaction sets us apart. Explore our showroom, browse our extensive inventory, and let our team of experts guide you towards your next set of wheels. Your journey towards automotive perfection begins here at Next Wheels. Start exploring today!</p>
+            <p className="hidden md:block">Welcome to TaskForge, where unparalleled task management meets seamless collaboration. Unleash the full potential of your productivity with our comprehensive system designed for businesses and individuals alike. Effortlessly organize, prioritize, and execute tasks with precision. Foster collaboration through intuitive features that streamline communication. Elevate your workflow experience and achieve more together. TaskForge - Transforming Tasks into Triumphs.</p>
           </div>
         </div>
       </div>
-      <div>
+      {/* <div>
         <Brands></Brands>
-      </div>
-      <div className="container mx-auto mb-32" data-aos="fade-right"
+      </div> */}
+      {/* <div className="container mx-auto mb-32" data-aos="fade-right"
      data-aos-offset="300"
      data-aos-easing="ease-in-sine">
         <h1 className="text-center text-5xl font-bold mb-16 text-[#00ADB5]">Most Asked Questions</h1>
         <FAQ></FAQ>
-      </div>
-      <div className="container mx-auto mb-32" data-aos="fade-right"
+      </div> */}
+      {/* <div className="container mx-auto mb-32" data-aos="fade-right"
      data-aos-offset="300"
      data-aos-easing="ease-in-sine">
         
         <Email></Email>
-      </div>
+      </div> */}
       
     </div>
   );
